3cd12d2cc4a58b03b0af1b1884f83d9d08840cf0
Jamie Yeh <jamie.yeh@disney.com>
2018-05-07 18:18:41 -0700

    adding working route and db connection

--- routes/rons.js
+++ routes/rons.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const mongoose = require('./../db/mongoose');
 const Ron = require('./../models/ron');
-const RonRouter = express.Router();
+const RonsRouter = express.Router();
 
 
-RonRouter.get('/', (req, res) => {
-  
+RonsRouter.get('/', (req, res) => {
+  Ron.find().then((rons) => {
+  res.render('rons', {
+    templateRons: rons
+  });
+}, (error) => {
+  res.status(400).send(error);
+  })
 });
+
+
+//HOW TO TROUBLESHOOT
+// RonsRouter.get('/', (req, res) => {
+//   //res.send("test rons");
+//   Ron.find().then((rons) => {
+//     console.log(rons);
+//   });
+// });
+
+
+RonsRouter.post('/', (req, res) => {
+  let newRon = new Ron({
+    quote: req.body.quote
+  });
+
+  newRon.save().then((ron) => {
+    res.redirect('/rons');
+  }, (error) => {
+    res.status(400).send("400 Bad Request");
+  });
+});
+
+
+module.exports = RonsRouter;
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const mongoose = require('mongoose');
-
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const RonRouter = require('./routes/rons');
+
+//resources
+const mongoose = require('mongoose');
+const RonsRouter = require('./routes/rons');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
+
+app.use('/rons', RonsRouter);
 
 app.set('view engine', 'ejs');
 
@@ -14,7 +31,6 @@ app.use(express.static( "public" ) );
 app.get('/', (req, res) => {
   res.render('home')
 });
-
 
 
 
