63d2162f379103cbd70ad729ed270c5732ff1020
Jamie Yeh <jamie.yeh@disney.com>
2018-05-08 10:33:49 -0700

    adding pulling external api quote working in console log

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 $(function () {
 
+  const $displayQuote = $('#external');
 
 
+  axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+    .then(response => console.log(response.data));
 
 });
