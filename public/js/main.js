24e49b44d1d5da3177e979ff99c6be8cf6c875aa
Jamie Yeh <jamie.yeh@disney.com>
2018-05-09 10:07:55 -0700

    adding generate random number, isEven function in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,30 @@ $(function () {
 
   const $displayQuote = $('#external');
 
-  // axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
-  //   .then(response => console.log(response.data));
-
-
   axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
     .then(response => {
 
       $('#external').text(`" ${response.data} "`);
       //console.log(response.data);
     });
+
+  //declare a variable for the random number by using math.random
+  const randomNum = Math.floor(Math.random() * 20);
+
+  const isEven = (number) => {
+    if (number % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+
+
+
+  //console.log(randomNum);
+    //if number is odd, pull from database
+    //create a click event on the Yes and No element
+
 
 });
